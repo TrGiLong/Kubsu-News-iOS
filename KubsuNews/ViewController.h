2aa2828a5cf6c6b2718bf35bedf04113de4a34e9
Long <trangianglong4003@gmail.com>
2017-03-23 21:16:27 +0300

    Initial Commit

---
+++ KubsuNews/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KubsuNews
+//
+//  Created by Long on 23.03.17.
+//  Copyright © 2017 darkTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KubsuNews/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  KubsuNews
+//
+//  Created by Long on 23.03.17.
+//  Copyright © 2017 darkTeam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
